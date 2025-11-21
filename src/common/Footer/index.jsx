70d323f0ff627d51f0f811b90fd4275f77104a10
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-11-21 17:48:21 +0530

    about chnages

--- src/common/Footer/index.jsx
+++ src/common/Footer/index.jsx
@@ -1,4 +1,5 @@
 import { MessageCircle, Phone, User } from "lucide-react";
+import Image from "next/image";
 import React from "react";
 // import { Linkedin, Instagram, Twitter } from "lucide-react";
 
@@ -7,14 +8,29 @@ const Footer = () => {
     <footer className="py-12 px-6 md:px-12 flex flex-col md:flex-row justify-between items-center text-xs uppercase tracking-widest text-[#888] font-body relative z-10 bg-[#0E0E0E]/80 backdrop-blur-sm border-t border-[#333]">
       <span>© 2025 Invictus Global Tech</span>
       <div className="flex gap-8 mt-6 md:mt-0">
-        <a href="#" className="hover:text-[#2AB182] transition-colors">
-          <User size={20} />
+        <a
+          href="https://www.instagram.com/invictusglobaltech/"
+          className="hover:text-[#2AB182] transition-colors"
+        >
+          <Image src={"/assets/instagram.png"} width={20} height={20} />
         </a>
-        <a href="#" className="hover:text-[#2AB182] transition-colors">
-          <Phone size={20} />
+        <a
+          href="https://www.facebook.com/people/Invictus-Global-Tech/61557394517942/"
+          className="hover:text-[#2AB182] transition-colors"
+        >
+          <Image src={"/assets/social-media.png"} width={20} height={20} />
         </a>
-        <a href="#" className="hover:text-[#2AB182] transition-colors">
-          <MessageCircle size={20} />
+        <a
+          href="https://www.youtube.com/@invictusglobaltech-offl"
+          className="hover:text-[#2AB182] transition-colors mt-1"
+        >
+          <Image src={"/assets/youtube.png"} width={20} height={20} />
+        </a>
+        <a
+          href="https://www.linkedin.com/company/invictus-global-tech/posts/?feedView=all"
+          className="hover:text-[#2AB182] transition-colors"
+        >
+          <Image src={"/assets/social.png"} width={20} height={20} />
         </a>
       </div>
     </footer>
