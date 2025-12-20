ea410d958a5a7bedf1fa9f543b1ad7a94e86f2fa
iyyappanmanivelan <iyyappanmani03@gmail.com>
2025-12-20 13:29:39 +0530

    added blueray ui page

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -13,7 +13,7 @@ export default function Document() {
           crossOrigin="anonymous"
         />
         <link
-          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Syne:wght@400;500;600;700;800&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&family=Syne:wght@400;500;600;700;800&family=Montserrat:wght@200;400;700&family=Roboto:wght@400;700&family=Open+Sans:wght@400;600&display=swap"
           rel="stylesheet"
         />
         <Script
